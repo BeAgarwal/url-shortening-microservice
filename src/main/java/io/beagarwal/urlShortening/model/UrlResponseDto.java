3e66fe39b807527aacf6ff5f9520cf8232219bd4
Shubham Agarwal <to.shubham.ag@gmail.com>
2021-05-03 02:39:18 +0530

    implemented UrlResponseDto model java class

--- src/main/java/io/beagarwal/urlShortening/model/UrlResponseDto.java
+++ src/main/java/io/beagarwal/urlShortening/model/UrlResponseDto.java
@@ -1,5 +1,51 @@
 package io.beagarwal.urlShortening.model;
 
-public class UrlResponseDto {
+import java.time.LocalDateTime;
 
+public class UrlResponseDto {
+    private String originalUrl;
+    private String shortLink;
+    private LocalDateTime expirationDate;
+
+    public UrlResponseDto(String originalUrl, String shortLink, LocalDateTime expirationDate) {
+        this.originalUrl = originalUrl;
+        this.shortLink = shortLink;
+        this.expirationDate = expirationDate;
+    }
+
+    public UrlResponseDto() {
+    }
+
+    public String getOriginalUrl() {
+        return originalUrl;
+    }
+
+    public void setOriginalUrl(String originalUrl) {
+        this.originalUrl = originalUrl;
+    }
+
+    public String getShortLink() {
+        return shortLink;
+    }
+
+    public void setShortLink(String shortLink) {
+        this.shortLink = shortLink;
+    }
+
+    public LocalDateTime getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(LocalDateTime expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    @Override
+    public String toString() {
+        return "UrlResponseDto{" +
+                "originalUrl='" + originalUrl + '\'' +
+                ", shortLink='" + shortLink + '\'' +
+                ", expirationDate=" + expirationDate +
+                '}';
+    }
 }
