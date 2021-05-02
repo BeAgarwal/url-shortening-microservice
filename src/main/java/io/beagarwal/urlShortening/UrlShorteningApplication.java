2d76848f81fa899a7b78406ba76d1aba1c04286e
Shubham Agarwal <to.shubham.ag@gmail.com>
2021-05-03 01:21:17 +0530

    main java spring boot file added

---
+++ src/main/java/io/beagarwal/urlShortening/UrlShorteningApplication.java
@@ -0,0 +1,13 @@
+package io.beagarwal.urlShortening;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShorteningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlShorteningApplication.class, args);
+	}
+
+}
