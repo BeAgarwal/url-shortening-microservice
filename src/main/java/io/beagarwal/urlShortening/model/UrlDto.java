3cfacac947e45a8f076c556926c0879759009514
Shubham Agarwal <to.shubham.ag@gmail.com>
2021-05-03 01:24:14 +0530

    added URLDto java class in model

--- src/main/java/io/beagarwal/urlShortening/model/UrlDto.java
+++ src/main/java/io/beagarwal/urlShortening/model/UrlDto.java
@@ -1,2 +1,42 @@
-package io.beagarwal.urlShortening.model;public class UrlDto {
+package io.beagarwal.urlShortening.model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class UrlDto {
+
+    private String url;
+    private String expirationDate;  //optional
+
+    public UrlDto(String url, String expirationDate) {
+        this.url = url;
+        this.expirationDate = expirationDate;
+    }
+
+    public UrlDto() {
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(String expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    @Override
+    public String toString() {
+        return "UrlDto{" +
+                "url='" + url + '\'' +
+                ", expirationDate='" + expirationDate + '\'' +
+                '}';
+    }
 }
