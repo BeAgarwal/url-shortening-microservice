bbffe267199983562e8707f95367194201826d2b
Shubham Agarwal <to.shubham.ag@gmail.com>
2021-05-03 01:23:51 +0530

    added URL java class in model

--- src/main/java/io/beagarwal/urlShortening/model/Url.java
+++ src/main/java/io/beagarwal/urlShortening/model/Url.java
@@ -1,2 +1,82 @@
-package io.beagarwal.urlShortening.model;public class Url {
+package io.beagarwal.urlShortening.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import java.time.LocalDateTime;
+
+@Entity
+public class Url {
+
+    @Id
+    @GeneratedValue
+    private long id;
+    @Lob
+    private String originalUrl;
+    private String shortLink;
+    private LocalDateTime creationDate;
+    private LocalDateTime expirationData;
+
+    public Url(long id, String originalUrl, String shortLink, LocalDateTime creationDate, LocalDateTime expirationData) {
+        this.id = id;
+        this.originalUrl = originalUrl;
+        this.shortLink = shortLink;
+        this.creationDate = creationDate;
+        this.expirationData = expirationData;
+    }
+
+    public Url() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getOriginalUrl() {
+        return originalUrl;
+    }
+
+    public void setOriginalUrl(String originalUrl) {
+        this.originalUrl = originalUrl;
+    }
+
+    public String getShortLink() {
+        return shortLink;
+    }
+
+    public void setShortLink(String shortLink) {
+        this.shortLink = shortLink;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public LocalDateTime getExpirationData() {
+        return expirationData;
+    }
+
+    public void setExpirationData(LocalDateTime expirationData) {
+        this.expirationData = expirationData;
+    }
+
+    @Override
+    public String toString() {
+        return "Url{" +
+                "id=" + id +
+                ", originalUrl='" + originalUrl + '\'' +
+                ", shortLink='" + shortLink + '\'' +
+                ", creationDate=" + creationDate +
+                ", expirationData=" + expirationData +
+                '}';
+    }
 }
